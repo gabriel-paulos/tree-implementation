457dcdfbd02b637729e2d95d20eab240be73aac8
fooo <gabrielp7700@gmail.com>
2023-04-17 23:38:00 -0400

    files

---
+++ featureDescriptor.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2 as cv
+
+class FeatureDescriptor():
+
+  def __init__(self,t="SIFT"):
+    self.sift = cv.SIFT_create()
+    self.mser = cv.MSER_create()
+    self.t = t
+
+  def extract_descriptors(self, img_path):
+
+    #according to paper they used both MSER and SIFT keypoints I will use only one,
+    #but use SIFT descriptors for each
+
+    img = cv.imread(img_path)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    if self.t == "SIFT":
+      kp, des = self.sift.detectAndCompute(img,None)
+    
+    elif self.t == "MSER":
+      kp = self.mser.detect(img)   
+      des = self.sift.compute(img, kp)  
+
+    return kp, des
+
+
